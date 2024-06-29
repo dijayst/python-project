a155e82b3453a55d3ed38b2f1e379cbb7e6a2da2
Esther <dijayst@gmail.com>
2024-06-29 10:02:31 +0100

    dep

--- Weatherapi.py
+++ Weatherapi.py
@@ -1,16 +1,18 @@
-#b17f7e1fa988f93d725139581b2d034a
+
 import requests
 import json
 
 # country_name = input("enter a the name of a country")
 
 def get_data(url):
-    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'})
-    
+    response = requests.get(url, 
+                            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'})
+    # A conditional statement to check if it connected to the api or not
+    # if its connected we only get some of the data which was printed in main_weather
     if response.status_code == 200:
-        data=response.json()
-       # print(data)
-       # print(response.text)
+        data = response.json()
+        print(data)
+        #print(response.text)
        
         main_weather = {
             "location_name": data.get('name'),
@@ -20,10 +22,10 @@ def get_data(url):
             "humidity": data.get('main').get('humidity'),
             "wind_speed": data.get('wind').get('speed')
         }
-        print(json.dumps(main_weather, indent=4))
+        #print(json.dumps(main_weather, indent=4))
       
     else:
         print(f"Failed to get data from {url}")
-        return []
+      
     
-get_data("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b17f7e1fa988f93d725139581b2d034a")
+get_data("https://api.openweathermap.org/data/2.5/weather?lat=6.60&lon=3.34&appid=b17f7e1fa988f93d725139581b2d034a")
---
+++ jsonwithflask.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Sample data
+data = [
+    {"id": 1, "name": "Alice", "role": "Engineer"},
+    {"id": 2, "name": "Bob", "role": "Designer"},
+    {"id": 3, "name": "Charlie", "role": "Manager"}
+]
+# here it get the json data in  /user
+@app.route('/users',)
+def get_users():
+    return jsonify(data)
+#checks if  a partficular name exist in the data and must be in lowercase
+@app.route('/users/<name>', methods=['GET'])
+def get_user_by_name(name):
+    user = next((user for user in data if user['name'].lower() == name.lower()), None)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({'error': 'User not found'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
